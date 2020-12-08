458967dcd092de0cb2ebc69421b377be55ff3d0d
Aiden Jeffrey <aiden.jeffrey@codethink.co.uk>
2020-12-08 18:59:41 +0000

    gpio: Add EINT interface

--- GPIO.h
+++ GPIO.h
@@ -17,6 +17,8 @@
 #define ERROR_PWM_UNSUPPORTED_DUTY_CYCLE (ERROR_SPECIFIC - 2)
 #define ERROR_PWM_UNSUPPORTED_CLOCK_SEL  (ERROR_SPECIFIC - 3)
 #define ERROR_PWM_NOT_A_PIN              (ERROR_SPECIFIC - 4)
+#define ERROR_EINT_NOT_A_PIN             (ERROR_SPECIFIC - 5)
+#define ERROR_EINT_ATTRIBUTE             (ERROR_SPECIFIC - 6)
 
 /// <summary>
 /// <para>Configure a pin for output. Call <see cref="GPIO_Write" /> to set the
@@ -67,6 +69,65 @@ int32_t GPIO_Read(uint32_t pin, bool *state);
 /// <param name="offTime">The number of clock cycles the PWM signal is low for.</param>
 /// <returns>Zero on success or an error value as defined in this file or Common.h.</returns>
 int32_t PWM_ConfigurePin(uint32_t pin, uint32_t clockFrequency, uint32_t onTime, uint32_t offTime);
+
+
+/* External Interrupts */
+
+#define GPIO_EINT_PIN_COUNT 24
+
+typedef enum {
+    GPIO_EINT_DBNC_FREQ_8KHZ   = 0,
+    GPIO_EINT_DBNC_FREQ_4KHZ   = 1,
+    GPIO_EINT_DBNC_FREQ_2KHZ   = 2,
+    GPIO_EINT_DBNC_FREQ_1KHZ   = 3,
+    GPIO_EINT_DBNC_FREQ_500HZ  = 4,
+    GPIO_EINT_DBNC_FREQ_250HZ  = 5,
+    GPIO_EINT_DBNC_FREQ_125HZ  = 6,
+    GPIO_EINT_DBNC_FREQ_62_5HZ = 7,
+    GPIO_EINT_DBNC_FREQ_INVALID
+} gpio_eint_dbnc_freq_e;
+
+/// <summary>
+/// <para>EINT configurations.</para>
+/// </summary>
+/// <param name="positive">Whether the interrupt triggers on logic high or not.</param>
+/// <param name="dualEdge">Whether to detect both transistions.</param>
+/// <param name="freq">Frequency of debounce counter.</param>
+typedef struct {
+    bool                  positive;
+    bool                  dualEdge;
+    gpio_eint_dbnc_freq_e freq;
+} gpio_eint_attr_t;
+
+static const gpio_eint_attr_t gpioEINTAttrDefault = {
+    .positive = true,
+    .dualEdge = false,
+    .freq     = GPIO_EINT_DBNC_FREQ_8KHZ
+};
+
+/// <summary>
+/// <para>Setup a GPIO pin to trigger an external interrupt.</para>
+/// </summary>
+/// <param name="pin">A specific pin, only pins < GPIO_EINT_PIN_COUNT are supported.</param>
+/// <param name="attr">Attributes to set - see struct docs above.</param>
+/// <returns>Zero on success or an error value as defined in this file or Common.h.</returns>
+int32_t EINT_ConfigurePin(
+    uint32_t           pin,
+    gpio_eint_attr_t  *attr);
+
+/// <summary>
+/// <para>Remove configuration for GPIO pin as EINT source.</para>
+/// </summary>
+/// <param name="pin">A specific pin, only pins < GPIO_EINT_PIN_COUNT are supported.</param>
+/// <returns>Zero on success or an error value as defined in this file or Common.h.</returns>
+int32_t EINT_DeConfigurePin(uint32_t pin);
+
+/// <summary>
+/// <para>Get debounce counter value.</para>
+/// </summary>
+/// <param name="pin">A specific pin, only pins < GPIO_EINT_PIN_COUNT are supported.</param>
+/// <returns>Debounce counter value.</returns>
+uint8_t EINT_GetDebounceCounter(uint32_t pin);
 
 #ifdef __cplusplus
  }
